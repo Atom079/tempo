a4b49c21fb40a36e7215f2202946f74e83f93a5a
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-04-16 06:23:44 -0800

    i have finished about section

--- assets/js/var.js
+++ assets/js/var.js
@@ -1,1 +1,5 @@
-'use strict';
+"use strict";
+window.addEventListener("scroll", ()=>{
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("active", window.scrollY > 0)
+})
