483f62164837e15829166a59a6ca5b1fd7ecb6ce
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-04-20 22:29:21 -0800

    Thank you everyone Alhamdilulloh

--- assets/js/var.js
+++ assets/js/var.js
@@ -1,13 +1,11 @@
 "use strict";
 const navbar = document.querySelector(".navbar");
 const homeBtn = document.querySelector(".home-btn");
-window.addEventListener("scroll", ()=>{  
-    navbar.classList.toggle("active", window.scrollY > 0)
-    homeBtn.classList.toggle("active", window.scrollY > 0)
-})
+window.addEventListener("scroll", () => {
+  navbar.classList.toggle("active", window.scrollY > 0);
+  homeBtn.classList.toggle("active", window.scrollY > 0);
+});
 const NavLinks = document.querySelectorAll(".navbar-nav li");
-NavLinks.forEach((item) =>{
-    item.addEventListener("click", ()=>{
-            
-    })
-})
+NavLinks.forEach((item) => {
+  item.addEventListener("click", () => {});
+});
