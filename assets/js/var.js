95db57013bd1350db28ee6926aba79894f872e83
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-04-20 22:04:15 -0800

    i have finished footer

--- assets/js/var.js
+++ assets/js/var.js
@@ -1,5 +1,7 @@
 "use strict";
 window.addEventListener("scroll", ()=>{
     const navbar = document.querySelector(".navbar");
+    const homeBtn = document.querySelector(".home-btn");
     navbar.classList.toggle("active", window.scrollY > 0)
+    homeBtn.classList.toggle("active", window.scrollY > 0)
 })
