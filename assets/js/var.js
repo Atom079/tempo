6fb6272c3746ae3368d2d249472500c07181985f
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-04-20 22:28:38 -0800

    i have done my work

--- assets/js/var.js
+++ assets/js/var.js
@@ -1,7 +1,13 @@
 "use strict";
-window.addEventListener("scroll", ()=>{
-    const navbar = document.querySelector(".navbar");
-    const homeBtn = document.querySelector(".home-btn");
+const navbar = document.querySelector(".navbar");
+const homeBtn = document.querySelector(".home-btn");
+window.addEventListener("scroll", ()=>{  
     navbar.classList.toggle("active", window.scrollY > 0)
     homeBtn.classList.toggle("active", window.scrollY > 0)
+})
+const NavLinks = document.querySelectorAll(".navbar-nav li");
+NavLinks.forEach((item) =>{
+    item.addEventListener("click", ()=>{
+            
+    })
 })
